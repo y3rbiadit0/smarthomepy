7e3dca6dc13c629850a8f0a6bdc3cc1bd14f5b93
Merenda Franco N <merenda.franco83@gmail.com>
2024-11-26 16:23:13 +0100

    US3 - Red Phase

--- test/test_smart_room.py
+++ test/test_smart_room.py
@@ -53,7 +53,7 @@ class TestSmartRoom(unittest.TestCase):
     @patch.object(SmartRoom, "check_enough_light")
     @patch.object(SmartRoom, "check_room_occupancy")
     @patch.object(GPIO, "output")
-    def test_manage_light_level(self, lightbulb_mock: Mock, check_room_occupancy_mock: Mock, check_enough_light_mock: Mock):
+    def test_manage_light_level_not_enough_light_and_person_in_the_room(self, lightbulb_mock: Mock, check_room_occupancy_mock: Mock, check_enough_light_mock: Mock):
         check_room_occupancy_mock.return_value = True
         check_enough_light_mock.return_value = False
 
@@ -61,6 +61,21 @@ class TestSmartRoom(unittest.TestCase):
 
         smart_room.manage_light_level()
         lightbulb_mock.assert_called_with(smart_room.LED_PIN, True)
+        self.assertTrue(smart_room.light_on)
 
+    @patch.object(SmartRoom, "check_enough_light")
+    @patch.object(SmartRoom, "check_room_occupancy")
+    @patch.object(GPIO, "output")
+    def test_manage_light_level_turn_off_otherwise(self, lightbulb_mock: Mock,
+                                check_room_occupancy_mock: Mock,
+                                check_enough_light_mock: Mock):
+        check_room_occupancy_mock.return_value = False
+        check_enough_light_mock.return_value = False
+
+        smart_room = SmartRoom()
+
+        smart_room.manage_light_level()
+        lightbulb_mock.assert_called_with(smart_room.LED_PIN, False)
+        self.assertFalse(smart_room.light_on)
 
 
